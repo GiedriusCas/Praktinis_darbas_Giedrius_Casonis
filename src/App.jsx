2d0897d973ac5cbdebe568fed4430e8fd9ef9363
Giedrius Casonis <giedriuscsn@gmail.com>
2023-04-20 19:02:21 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/reset.css';
+import './styles/App.css';
+import Grid from './components/ui/grid/Grid';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>New project</h1>
+    </div>
+  );
+}
+export default App;
