d8a74801e7da22e94d660bc29daf37618bb0debb
Giedrius Casonis <giedriuscsn@gmail.com>
2023-04-21 22:20:46 +0300

    getFirestore

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import './header.scss'
+import './header.scss';
 import { useAuthCtx } from '../../store/AuthProvider';
 
 function Header() {
-  const {isLoggedIn} = useAuthCtx();
+  const { isLoggedIn } = useAuthCtx();
   return (
     <header>
       <div className="container">
-        <Link to={"/"} className="logo">
-          Logo
+        <Link to={'/'} className="logo" hrefLang="">
+          <img className="shopLogo" src="/public/SHOPS (3).jpg" alt="logo" />
         </Link>
         <nav>
-          <NavLink className='navItem' to={'/'}>Home page</NavLink>
-          <NavLink className='navItem' to={'/login'}>Login</NavLink>
-          <NavLink className='navItem' to={'/regist'}>Register</NavLink>
-          {isLoggedIn &&(<NavLink className='navItem' to={'/shops'}>Shops</NavLink>)}
-          {isLoggedIn &&(<NavLink className='navItem' to={'/add'}>Add Shop</NavLink>)}
-          
+          <NavLink className="navItem" to={'/'}>
+            Home page
+          </NavLink>
+          <NavLink className="navItem" to={'/login'}>
+            Login
+          </NavLink>
+          <NavLink className="navItem" to={'/regist'}>
+            Register
+          </NavLink>
+          {isLoggedIn && (
+            <NavLink className="navItem" to={'/shops'}>
+              Shops
+            </NavLink>
+          )}
+          {isLoggedIn && (
+            <NavLink className="navItem" to={'/add'}>
+              Add Shop
+            </NavLink>
+          )}
         </nav>
       </div>
     </header>
--- src/pages/ShopsPage.jsx
+++ src/pages/ShopsPage.jsx
@@ -1,10 +1,43 @@
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase/firebase';
+import { useEffect, useState } from 'react';
+
 function ShopsPage() {
-    return (
-      <div>
-          <h1>ShopsPage</h1>
-          <p>This is Shopspage</p>
-      </div>
-    )
-  }
-  
-  export default ShopsPage
+  const [shopsArr, setShopsArr] = useState([]);
+
+  useEffect(() => {
+    async function getShops() {
+      const querySnapshot = await getDocs(collection(db, 'shops'));
+      const tempShops = [];
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        tempShops.push({uid: doc.id, ...doc.data()})
+      });
+    console.log('tempShops ===', tempShops);
+      setShopsArr(tempShops)
+    }
+    getShops();
+  }, []);
+
+  return (
+    <div>
+      <h1>ShopsPage</h1>
+      <p>This is Shopspage</p>
+      <ul>
+        {shopsArr.map((pObj) =>(
+          <li key={pObj.uid}>
+            <img src={pObj.imageUrl} alt="shop logo" />
+            <div>
+            <h3>{pObj.shopName}</h3>
+            <p>{pObj.description}</p>
+            <p>{pObj.town}</p>
+            <p>{pObj.startYear}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ShopsPage;
