547006eab7990ec6970d9b2181ed2d91be60b33a
Giedrius Casonis <giedriuscsn@gmail.com>
2023-04-24 13:00:43 +0300

    login with google

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,17 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import {
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from 'firebase/auth';
 import LoginForm from '../components/auth/LoginForm';
-import { auth } from '../firebase/firebase';
+import { auth, googleProvider } from '../firebase/firebase';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuthCtx } from '../store/AuthProvider';
+import './loginPage.scss';
 
 function LoginPage() {
-  
   const navigate = useNavigate();
-const { login, ui, setIsLoading } = useAuthCtx();
+  const { login, ui, setIsLoading } = useAuthCtx();
   function loginUser({ email, password }) {
     ui.showLoading();
     setIsLoading(true);
@@ -18,10 +22,39 @@ const { login, ui, setIsLoading } = useAuthCtx();
         login(user);
         setIsLoading(false);
         navigate('/shops');
-
       })
       .catch((error) => {
         const errorMessage = error.message;
+        ui.showError(errorMessage);
+        setIsLoading(false);
+      });
+  }
+  function loginWithGoogle() {
+    //
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+        console.log('user ===', user);
+        login(user);
+        setIsLoading(false);
+        navigate('/shops');
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.warn('errorMessage ===', errorMessage);
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
         ui.showError(errorMessage);
         setIsLoading(false);
       });
@@ -30,8 +63,10 @@ const { login, ui, setIsLoading } = useAuthCtx();
     <div className="container">
       <h1>LoginPage</h1>
       <p>This is Loginpage</p>
-      <LoginForm onLogin={loginUser} />
       <Link to={'/regist'}>New user? Register here...</Link>
+      <LoginForm onLogin={loginUser} />
+      <p>or</p>
+      <button onClick={loginWithGoogle}>Login with Google account</button>
     </div>
   );
 }
